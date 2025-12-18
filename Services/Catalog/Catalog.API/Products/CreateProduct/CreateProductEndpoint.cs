4c05a58f205181bce29f6353c8ddb8ba82be5393
Ahmed Gamal Ghazi <agmghazi@hotmail.com>
2025-12-18 16:05:21 +0300

    Add Carter, MediatR, Mapster; implement CreateProduct endpoint
    Introduce Carter for modular endpoints in Catalog.API. Add MediatR and Mapster to BuildingBlocks for CQRS and object mapping. Implement CreateProductEndpoint with POST /products using Carter, MediatR, and Mapster. Update Program.cs to register and map Carter endpoints. Add global usings for Carter, Mapster, and MediatR.

--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, decimal Price);
+public record CreateProductResponse(Guid id);
+public class CreateProductEndpoint : ICarterModule
 {
-    public class CreateProductEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/products",
+           async (CreateProductRequest requset, ISender sender) =>
+           {
+               var command = requset.Adapt<CreateProductCommand>();
+               var result = await sender.Send(command);
+               var respnse = result.Adapt<CreateProductResponse>();
+               return Results.Created($"/products/{respnse.id}", respnse);
+           })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Creates a new product")
+            .WithDescription("Creates a new product with the provided details.");
     }
 }
--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapCarter();
 
 app.Run();
