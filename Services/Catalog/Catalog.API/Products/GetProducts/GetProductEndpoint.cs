87313756356ea520bf56c4f3fd5ed85f2b6fb035
Ahmed Gamal Ghazi <agmghazi@hotmail.com>
2025-12-23 16:09:41 +0300

    Add GetProducts endpoint, update Docker and config
    - Added GetProductEndpoint and handler for retrieving all products.
    - Updated docker-compose.yml: switched Postgres image, added pgAdmin, exposed ports, and enabled catalog.api service.
    - Changed database name in appsettings.json to lowercase.
    - Adjusted HTTPS port in launchSettings.json.
    - Cleaned up port mappings in docker-compose.override.yml.

---
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductRequest(IEnumerable<Product> Products);
+    public class GetProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/GetProducts",
+               async (ISender sender) =>
+               {
+                   var query = new GetProductsQuery();
+                   var result = await sender.Send(query);
+                   var response = result.Adapt<GetProductRequest>();
+                   return Results.Ok(response);
+               })
+             .WithName("GetProducts")
+             .Produces<GetProductRequest>(StatusCodes.Status200OK)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .WithSummary("Gets all products")
+             .WithDescription("Retrieves a list of all products available in the catalog.");
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery(): IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+
+    internal class GetProductQueryHandler
+        (IDocumentSession session, ILogger<GetProductQueryHandler> logger)
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(
+            GetProductsQuery query,
+            CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling GetProductsQuery");
+            var products = await session
+                .Query<Product>()
+                .ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+        }
+    }
+}
